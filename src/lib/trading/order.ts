9450dcefebb1897663013964eab4b09f9734280d
newbizme <31482401+newbizme@users.noreply.github.com>
2018-10-20 20:24:59 +0700

    Update order.ts

--- src/lib/trading/order.ts
+++ src/lib/trading/order.ts
@@ -17,12 +17,12 @@ export class Order extends ApiHandler {
   async orderA(exchange: types.IExchange, testTrade: types.ITradeTriangle) {
     try {
       const timer = Helper.getTimer();
-      // 已下单时跳过
+      // Пропустить при размещении заказа
       if (!testTrade.a.orderId) {
-        // 获取交易额度
-        logger.info(`第一步：${clc.blueBright(testTrade.a.pair)}`);
+        // Получить сумму транзакции
+        logger.info(`Первый шаг：${clc.blueBright(testTrade.a.pair)}`);
         testTrade.a.timecost = '';
-        logger.info(`限价：${testTrade.a.price}, 数量：${testTrade.a.amount}, 方向：${testTrade.a.side}`);
+        logger.info(`Предельная цена：${testTrade.a.price}, количество：${testTrade.a.amount}, направление：${testTrade.a.side}`);
         const order = <types.IOrder>{
           symbol: testTrade.a.pair,
           side: testTrade.a.side.toLowerCase(),
@@ -34,28 +34,28 @@ export class Order extends ApiHandler {
         if (!orderInfo) {
           return;
         }
-        logger.debug(`下单返回值: ${JSON.stringify(orderInfo, null, 2)}`);
+        logger.debug(`Возвращаемое значение заказа: ${JSON.stringify(orderInfo, null, 2)}`);
 
         testTrade.a.status = orderInfo.status;
         testTrade.a.orderId = orderInfo.id;
 
-        // 更新队列
+        // Обновить очередь
         await this.storage.updateTradingSession(testTrade, 0);
       }
       const nextB = async () => {
-        logger.info('执行nextB...');
+        logger.info('выполнение nextB...');
         const orderRes = await this.queryOrder(exchange, testTrade.a.orderId, testTrade.a.pair);
         if (!orderRes) {
           return false;
         }
-        logger.info(`查询订单状态： ${orderRes.status}`);
-        // 交易成功时
+        logger.info(`Статус заказа запроса： ${orderRes.status}`);
+        // Когда транзакция успешна
         if (orderRes.status === 'closed') {
           testTrade.a.timecost = Helper.endTimer(timer);
-          // 修正数量
+          // Исправленное количество
           testTrade.a.amount = orderRes.amount;
           testTrade.a.status = orderRes.status;
-          // 更新队列
+          // Обновить очередь
           await this.storage.updateTradingSession(testTrade, 0);
 
           if (this.worker) {
@@ -67,14 +67,14 @@ export class Order extends ApiHandler {
         return false;
       };
 
-      // 订单未成交时
+      // Когда заказ не заполнен
       if (!await nextB()) {
-        logger.info('订单未成交,每秒循环执行。');
+        logger.info('Заказ не заполняется и циклически выполняется каждую секунду.');
         this.worker = setInterval(nextB.bind(this), 1000);
       }
     } catch (err) {
       const errMsg = err.message ? err.message : err.msg;
-      logger.error(`订单A出错： ${errMsg}`);
+      logger.error(`Ошибка заказа： ${errMsg}`);
       await this.errorHandle(testTrade.queueId, errMsg);
     }
   }
@@ -83,11 +83,11 @@ export class Order extends ApiHandler {
     try {
       const timer = Helper.getTimer();
       const tradeB = trade.b;
-      // 已下单时跳过
+      // Пропустить при размещении заказа
       if (!tradeB.orderId) {
 
-        logger.info(`第二步：${clc.blueBright(trade.b.pair)}`);
-        logger.info(`限价：${tradeB.price}, 数量：${tradeB.amount}, 方向：${tradeB.side}`);
+        logger.info(`Второй шаг：${clc.blueBright(trade.b.pair)}`);
+        logger.info(`Предельная цена：${tradeB.price}, количество：${tradeB.amount}, направление：${tradeB.side}`);
         const order = <types.IOrder>{
           symbol: tradeB.pair,
           side: tradeB.side.toLowerCase(),
@@ -99,31 +99,31 @@ export class Order extends ApiHandler {
         if (!orderInfo) {
           return;
         }
-        logger.debug(`下单返回值: ${JSON.stringify(orderInfo, null, 2)}`);
+        logger.debug(`Возвращаемое значение заказа: ${JSON.stringify(orderInfo, null, 2)}`);
 
         trade.b.status = <any>orderInfo.status;
         trade.b.orderId = orderInfo.id;
-        // 更新队列
+        // Обновить очередь
         await this.storage.updateTradingSession(trade, 1);
       }
       const nextC = async () => {
-        logger.info('执行nextC...');
+        logger.info('выполнение nextC...');
 
         const orderRes = await this.queryOrder(exchange, tradeB.orderId, tradeB.pair);
         if (!orderRes) {
           return false;
         }
-        logger.info(`查询订单状态： ${orderRes.status}`);
-        // 交易成功时
+        logger.info(`Статус заказа запроса： ${orderRes.status}`);
+        // Когда транзакция успешна
         if (orderRes.status === 'closed') {
           if (this.worker) {
             clearInterval(this.worker);
           }
           trade.b.timecost = Helper.endTimer(timer);
-          // 修正数量
+          // Исправленное количество
           trade.b.amount = orderRes.amount;
           trade.b.status = orderRes.status;
-          // 更新队列
+          // Обновить очередь
           await this.storage.updateTradingSession(trade, 1);
           await this.orderC(exchange, trade);
           return true;
@@ -131,14 +131,14 @@ export class Order extends ApiHandler {
         return false;
       };
 
-      // 订单未成交时
+      // Когда заказ не заполнен
       if (!await nextC()) {
-        logger.info('订单未成交,每秒循环执行。');
+        logger.info('Заказ не заполняется и циклически выполняется каждую секунду');
         this.worker = setInterval(nextC.bind(this), 1000);
       }
     } catch (err) {
       const errMsg = err.message ? err.message : err.msg;
-      logger.error(`订单B出错： ${errMsg}`);
+      logger.error(`Ошибка заказа B： ${errMsg}`);
       await this.errorHandle(trade.queueId, errMsg);
     }
   }
@@ -147,10 +147,10 @@ export class Order extends ApiHandler {
     try {
       const timer = Helper.getTimer();
       const tradeC = trade.c;
-      // 已下单时跳过
+      // Пропустить при размещении заказа
       if (!tradeC.orderId) {
-        logger.info(`第三步：${clc.blueBright(trade.c.pair)}`);
-        logger.info(`限价：${tradeC.price}, 数量：${tradeC.amount}, 方向：${tradeC.side}`);
+        logger.info(`Третий этап：${clc.blueBright(trade.c.pair)}`);
+        logger.info(`Предельная цена：${tradeC.price}, количество：${tradeC.amount}, направление：${tradeC.side}`);
         if (tradeC.side.toLowerCase() === 'sell' && tradeC.amount > trade.b.amount) {
           tradeC.amount = trade.b.amount;
         }
@@ -165,11 +165,11 @@ export class Order extends ApiHandler {
         if (!orderInfo) {
           return;
         }
-        logger.debug(`下单返回值: ${JSON.stringify(orderInfo, null, 2)}`);
+        logger.debug(`Возвращаемое значение заказа: ${JSON.stringify(orderInfo, null, 2)}`);
 
         trade.c.status = orderInfo.status;
         trade.c.orderId = orderInfo.id;
-        // 更新队列
+        // Обновить очередь
         await this.storage.updateTradingSession(trade, 2);
       }
       const completedC = async () => {
@@ -178,31 +178,31 @@ export class Order extends ApiHandler {
         if (!orderRes) {
           return false;
         }
-        logger.info(`查询订单状态： ${orderRes.status}`);
-        // 交易成功时
+        logger.info(`Статус заказа запроса： ${orderRes.status}`);
+        // Когда транзакция успешна
         if (orderRes.status === 'closed') {
           if (this.worker) {
             clearInterval(this.worker);
           }
-          logger.info(`三角套利完成,最终获得：${orderRes.amount}...`);
+          logger.info(`Треугольный арбитраж завершен и, наконец, получен：${orderRes.amount}...`);
           trade.c.timecost = Helper.endTimer(timer);
-          // 修正数量
+          // Исправленное количество
           trade.c.amount = orderRes.amount;
           trade.c.status = orderRes.status;
-          // 在交易队列中清除这条数据
+          // Очистить эти данные в очереди транзакций
           await this.storage.closeTradingSession(trade);
         }
         return false;
       };
 
-      // 订单未成交时
+      // Когда заказ не заполнен
       if (!await completedC()) {
-        logger.info('订单未成交,每秒循环执行。');
+        logger.info('Заказ не заполняется и циклически выполняется каждую секунду.');
         this.worker = setInterval(completedC.bind(this), 1000);
       }
     } catch (err) {
       const errMsg = err.message ? err.message : err.msg;
-      logger.error(`订单C出错： ${errMsg}`);
+      logger.error(`Ошибка заказа C： ${errMsg}`);
       await this.errorHandle(trade.queueId, errMsg);
     }
   }
